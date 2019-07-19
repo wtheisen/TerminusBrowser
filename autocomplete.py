0b00fe4f68028d547b6ea9c4e3fd41e0a674dced
Gavin Inglis <ginglis05@gmail.com>
2019-07-19 19:19:41 -0400

    autocomplete.py: toggle autocomplete

--- autocomplete.py
+++ autocomplete.py
@@ -23,14 +23,15 @@ def autoComplete(editBox):
     if len(inputList) == 1: #completing first bit
         currCommand = currText.split()[0]
         matches = [x for x in commandList if x.startswith(currCommand)]
-        try:
-            match = min(matches, key=len)
-            if currCommand in commandList:
-                try:
-                    match = matches[1]
-                except IndexError:
-                    pass
-            editBox.set_edit_text(match)
-            editBox.set_edit_pos(len(match))
-        except ValueError:
-            pass
+        # Check to toggle for previous match
+        if len(matches) == 1:
+            currCommand = currCommand[:3]
+            matches = [x for x in commandList if x.startswith(currCommand)]
+        match = min(matches, key=len)
+        if currCommand in commandList:
+            try:
+                match = matches[1]
+            except IndexError:
+                pass
+        editBox.set_edit_text(match)
+        editBox.set_edit_pos(len(match))
