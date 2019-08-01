f1ff1d2b4ce3f9996c71c82819f36833e77669be
Gavin Inglis <ginglis05@gmail.com>
2019-07-31 22:07:07 -0400

    test: autocomplete

---
+++ tests/test_autocomplete.py
@@ -0,0 +1,24 @@
+# test_autocomplete.py
+
+from customUrwidClasses import CommandBar
+from commandChanVim import urwidView
+from autocomplete import autoComplete
+
+import pytest
+
+test_list = [
+    ('too long command', 'too long command'),
+    ('sti', 'stickies'), # input len 3
+    ('po', 'post'), # input len 2
+    ('re', 'reply'), # 2 cmds start with re
+    ('threadStyle', 'thread'), # toggle check
+    ('qsuaik', 'qsuaik') # gibberish
+]
+
+@pytest.mark.parametrize("test_input, expected", test_list)
+def test_CommandBar(test_input, expected):
+    uvm = urwidView()
+    cb = CommandBar(lambda: uvm._update_focus(True), uvm)
+    cb.set_edit_text(test_input)
+    autoComplete(cb)
+    assert cb.get_edit_text() == expected
