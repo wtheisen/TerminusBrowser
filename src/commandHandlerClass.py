2eeef4b6f9cd6b8cc133617859273d660643be3c
mpernigo <marco.pernigotti0@gmail.com>
2020-10-03 18:02:29 +0200

    commands: add command_map to dispatch commands

--- src/commandHandlerClass.py
+++ src/commandHandlerClass.py
@@ -11,7 +11,18 @@ from customeTypes import SITE, MODE
 import logging
 log = logging.getLogger(__name__)
 
+
 class CommandHandler:
+    command_map = {
+        str(SITE.REDDIT): redditCommands,
+        str(SITE.FCHAN): chanCommands,
+        str(SITE.LCHAN): chanCommands,
+        str(SITE.HACKERNEWS): hnCommands,
+        str(SITE.LOBSTERS): lobsterCommands,
+    }
+
+    command_set = set(SystemCommandList + ChanCommandList + RedditCommandList + HNCommandList + LobsterCommandList)
+
     def __init__(self, uvm):
         self.defLeadChar = ':'
         self.uvm = uvm
@@ -31,17 +42,16 @@ class CommandHandler:
 
         log.debug(cmd)
 
-        if cmd.split()[0] not in (SystemCommandList + ChanCommandList + RedditCommandList + HNCommandList + LobsterCommandList):
+        if cmd.split()[0] not in self.command_set:
             return
         elif cmd.split()[0] in SystemCommandList:
             systemCommands(cmd, self.uvm)
-        elif cmd.split()[0] in ChanCommandList:
-            chanCommands(cmd, self.uvm)
-        elif cmd.split()[0] in RedditCommandList:
-            redditCommands(cmd, self.uvm)
-        elif cmd.split()[0] in HNCommandList:
-            hnCommands(cmd, self.uvm)
-        elif cmd.split()[0] in LobsterCommandList:
-            lobsterCommands(cmd, self.uvm)
+        else:
+            try:
+                command_scope = self.command_map[str(self.uvm.currFocusView.site)]
+            except KeyError as e:
+                log.debug("command does not exist {e}")
+            else:
+                command_scope(cmd, self.uvm)
 
         self.postCommand()
