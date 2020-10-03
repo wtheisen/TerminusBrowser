b800835836e3e326e70a3cb7b193f36c6b1d0f40
mpernigo <marco.pernigotti0@gmail.com>
2020-10-03 18:03:07 +0200

    tests: adapt existing tests to use command_map

--- tests/test_reddit.py
+++ tests/test_reddit.py
@@ -8,15 +8,22 @@ from TerminusBrowser import urwidView
 from Frames.reddit.indexFrame import RedditIndexFrame
 from Frames.reddit.subredditFrame import SubredditFrame
 
+from src.customeTypes import SITE
+
 import pytest
 
+
 test_list = [
-    ('view reddit', RedditIndexFrame),
-    ('sub /r/all', SubredditFrame),
+    ('view reddit', None, RedditIndexFrame),
+    ('sub /r/all', SITE.REDDIT, SubredditFrame),
 ]
 
-@pytest.mark.parametrize('test_input, expected', test_list)
-def test_fchan(test_input, expected):
+
+@pytest.mark.parametrize('test_input,site,expected', test_list)
+def test_reddit(test_input, site, expected):
     uvm = urwidView(True)
+    uvm.currFocusView.site = site
+
     uvm.commandHandler.routeCommand(test_input)
+
     assert type(uvm.currFocusView.frame) == expected
